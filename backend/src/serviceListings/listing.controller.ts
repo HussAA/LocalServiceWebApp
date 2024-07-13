5b56a6503a56b62b9042df040587891ec43efb1e
hussaa <mah.hussaina@gmail.com>
2024-07-13 06:50:06 -0400

    saved

--- backend/src/serviceListings/listing.controller.ts
+++ backend/src/serviceListings/listing.controller.ts
@@ -7,13 +7,10 @@ import {
   Body,
   Param,
   Req,
-  UseGuards,
 } from '@nestjs/common';
 import { ListingService } from './listing.service';
 import { Listing } from './listing.entity';
-import { LocalAuthGuard } from '../auth/local-auth.guard'; // Adjust the path as necessary
 import { User } from '../users/user.entity'; // Adjust the path as necessary
-import { JwtAuthGuard } from '../auth/jwt-auth.guard'; 
 @Controller('listings')
 export class ListingController {
   constructor(private readonly listingService: ListingService) {}
@@ -27,7 +24,6 @@ export class ListingController {
   async findOne(@Param('id') id: number): Promise<Listing> {
     return this.listingService.findOne(id);
   }
-
 
   @Post()
   async create(
@@ -44,14 +40,13 @@ export class ListingController {
     @Body() updateData: Partial<Listing>,
   ): Promise<Listing> {
     const user: User = req.user;
-    return this.listingService.update(user, id, updateData)
+    return this.listingService.update(user, id, updateData);
   }
 
   @Delete(':id')
   async remove(@Req() req, @Param('id') id: number): Promise<void> {
     const user: User = req.user;
-    console.log("this is delete", user, id)
+    console.log('this is delete', user, id);
     return this.listingService.remove(user, id);
-    
   }
 }
--- backend/src/serviceListings/listing.service.ts
+++ backend/src/serviceListings/listing.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Inject, NotFoundException, ForbiddenException } from '@nestjs/common';
+import {
+  Injectable,
+  Inject,
+  NotFoundException,
+  ForbiddenException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Listing } from './listing.entity';
 import { User } from '../users/user.entity';
@@ -15,7 +20,10 @@ export class ListingService {
   }
 
   async findOne(id: number): Promise<Listing> {
-    const listing = await this.listingRepository.findOne({ where: { id }, relations: ['user'] });
+    const listing = await this.listingRepository.findOne({
+      where: { id },
+      relations: ['user'],
+    });
     if (!listing) {
       throw new NotFoundException(`Listing with ID ${id} not found`);
     }
@@ -31,25 +39,33 @@ export class ListingService {
     return await this.listingRepository.save(listing);
   }
 
-  async update(user: User, id: number, updateData: Partial<Listing>): Promise<Listing> {
+  async update(
+    user: User,
+    id: number,
+    updateData: Partial<Listing>,
+  ): Promise<Listing> {
     const listing = await this.findOne(id);
-    
+
     if (listing.user.id !== user.id) {
-      throw new ForbiddenException('You are not allowed to update this listing');
+      throw new ForbiddenException(
+        'You are not allowed to update this listing',
+      );
     }
-  
+
     await this.listingRepository.update(id, updateData);
-  
+
     return this.findOne(id);
   }
 
   async remove(user: User, id: number): Promise<void> {
     const listing = await this.findOne(id);
-    console.log("this is remove", listing)
+    console.log('this is remove', listing);
     if (listing.user.id !== user.id) {
-      throw new ForbiddenException('You are not allowed to delete this listing');
+      throw new ForbiddenException(
+        'You are not allowed to delete this listing',
+      );
     }
-    
+
     const result = await this.listingRepository.delete(id);
     if (result.affected === 0) {
       throw new NotFoundException(`Listing with ID ${id} not found`);
